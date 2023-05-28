6b203196fbcb584e6afbf7ed3e13ae83781089a7
Martin <martingrunwald91@gmail.com>
2023-05-28 22:46:09 +0200

    even more turtles

--- themes/common.theme.zsh
+++ themes/common.theme.zsh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
 PROMPT="$PVAR[firstnl]["
-PROMPT+="`clr -r ${PVAR[time]::-3} 35 1` "
+PROMPT+="`clr -r ${PVAR[time]::-3} 32 1` "
 PROMPT+="`clr -r %n 36 1`@`clr -r ${PVAR[conn_ip]} 32 1` "
 PROMPT+="&`clr -r $PVAR[histmode] 36 1 3` `clr -r %c 32 1`"
 PROMPT+="]"
-PROMPT+="$(clr -r "$PVAR[note]" 3)"
-PROMPT+="$(clr -r "$PVAR[ls]"   2)"
-PROMPT+="$PVAR[nl]%# "
+PROMPT+="$(clr -r "$PVAR[note]" 3 32)"
+PROMPT+="$(clr -r "$PVAR[ls]"   2 36)"
+PROMPT+="$PVAR[nl]🐢`clr -r %# 1 32` "
 
 SIMPLE_PROMPT="[&`clr -r $PVAR[histmode] 36 1 3 ` `clr -r %c 32 1`]%# "
 
