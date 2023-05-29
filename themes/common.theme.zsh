c72fe374d881bc9062f95ef6d1636e78e3ddf2f3
Martin <martingrunwald91@gmail.com>
2023-05-29 14:45:03 +0200

    Created command `tinfo` to show system information in tmux panes

--- themes/common.theme.zsh
+++ themes/common.theme.zsh
@@ -1,15 +1,17 @@
 #!/bin/zsh
 
 PROMPT="$PVAR[firstnl]["
-PROMPT+="`clr -r ${PVAR[time]::-3} 32 1` "
-PROMPT+="`clr -r %n 36 1`@`clr -r ${PVAR[conn_ip]} 32 1` "
-PROMPT+="&`clr -r $PVAR[histmode] 36 1 3` `clr -r %c 32 1`"
+PROMPT+="`clr -r time= 2``clr -r ${PVAR[time]::-3} 32 1` "
+PROMPT+="`clr -r user= 2``clr -r %n 36 1` "
+PROMPT+="`clr -r ip= 2``clr -r ${PVAR[conn_ip]} 32 1` "
+PROMPT+="`clr -r history= 2``clr -r $PVAR[histmode] 36 1 3` "
+PROMPT+="`clr -r pwd= 2``clr -r %c 32 1`"
 PROMPT+="]"
 PROMPT+="$(clr -r "$PVAR[note]" 3 32)"
 PROMPT+="$(clr -r "$PVAR[ls]"   2 36)"
-PROMPT+="$PVAR[nl]🐢`clr -r . 1 32`%# "
+PROMPT+="$PVAR[nl]🐢`clr -r %# 1 32`> "
 
-SIMPLE_PROMPT="[&`clr -r $PVAR[histmode] 36 1 3 ` `clr -r %c 32 1`]%# "
+SIMPLE_PROMPT="`clr -r %n 32 1 `[`clr -r $PVAR[histmode] 36 1 3 `] `clr -r %c 32 1`%# "
 
 export PROMPT
 export SIMPLE_PROMPT
